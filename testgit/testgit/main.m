a2ddab37fc5c33d168646d951fa7d35ff90034a6
kris <kris@krisdemacbook-pro.local>
2016-04-14 23:13:22 +0800

    asdfasf

---
+++ testgit/testgit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testgit
+//
+//  Created by kris on 16/4/14.
+//  Copyright © 2016年 kris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
